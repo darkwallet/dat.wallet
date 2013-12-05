1474ac29e6079dbe935f6a2617d5b76365821869
Taylor Gerring <taylor.gerring@gmail.com>
2013-12-05 16:04:46 +0100

    Refactored widget sections into discrete classes

--- main.py
+++ main.py
@@ -24,35 +24,6 @@ class RootWidget(BoxLayout):
     pass
 
 
-def get_transactions(start=0, amount=100):
-    item_strings=[('+500 mBTC sent to 1MNmTP...') for index in range(amount)]
-
-    return item_strings
-
-def copy_address_to_clipboard(instance):
-    address = instance.next_address
-    clipboard.copy(address)
-    show_copied_popup(address)
-    print Clipboard.get('UTF8_STRING')
-
-def show_copied_popup(address):
-    btnclose = Button(text='Close this popup', size_hint_y=None, height='50sp')
-    content = BoxLayout(orientation='vertical')
-    row_length = 15
-    for i in range(0, len(address), row_length):
-        content.add_widget(Label(text=address[i:i + row_length]))
-    content.add_widget(btnclose)
-    popup = Popup(content=content, title='Copied Address',
-                  size_hint=(None, None), size=('300dp', '300dp'))
-    btnclose.bind(on_release=popup.dismiss)
-    button = Button(text='Open popup', size_hint=(None, None),
-                    size=('150sp', '70dp'),
-                    on_release=popup.open)
-    popup.open()
-    col = AnchorLayout()
-    col.add_widget(button)
-    return col
-
 class SendSection(BoxLayout):
     def __init__(self, **kwargs):
         super(SendSection, self).__init__(**kwargs)
@@ -82,46 +53,101 @@ class SendSection(BoxLayout):
         # TODO validate address
         # TODO call backend send function
 
-class MainApp(App):
-
-    def build(self):
-        self.backend = backend.Backend(seedphrase)
-        root = RootWidget()
+class ReceiveSection(BoxLayout):
+    def __init__(self, **kwargs):
+        super(ReceiveSection, self).__init__(**kwargs)
 
         main_layout = BoxLayout(orientation='vertical')
 
-        ### balance section
-        balance = BoxLayout(orientation='horizontal', size_hint_y=0.6)
-        # TODO wire up the real amount to a formatter which gets fed here
-        balance.add_widget(Label(text='1,276.00 mBTC', font_size=28, bold=True))
-        main_layout.add_widget(balance)
-        ### end balance section
 
-        ### next address section
         nextaddress_label = Label(text='click below to copy\nnext receive address',
             size_hint_y=0.5, font_size=20, halign='center')
         main_layout.add_widget(nextaddress_label)
 
         # TODO wire this text input up to a real address which may change when a transaction to this address occurs
-        current_address = self.backend.current_address
-        shortened_address = current_address[:6] + "..."
+        self.current_address = backend.current_address
+        shortened_address = self.current_address[:6] + "..."
         nextaddress_input = Button(
             text=shortened_address,
             size_hint_y=0.3,
             font_size=20,
             foreground_color=(1,1,1,1))
-        nextaddress_input.bind(on_press=copy_address_to_clipboard)
-        nextaddress_input.next_address = current_address
+        nextaddress_input.bind(on_press=self.copy_address_to_clipboard)
+        nextaddress_input.next_address = self.current_address
         main_layout.add_widget(nextaddress_input)
-        ### end next address section
 
-        main_layout.add_widget(SendSection())
+        self.add_widget(main_layout)
 
-        # ##transaction section
+
+    def copy_address_to_clipboard(self, instance):
+        address = instance.next_address
+        clipboard.copy(address)
+        self.show_copied_popup(address)
+        print Clipboard.get('UTF8_STRING')
+
+    def show_copied_popup(self, address):
+        btnclose = Button(text='Close this popup', size_hint_y=None, height='50sp')
+        content = BoxLayout(orientation='vertical')
+        row_length = 15
+        for i in range(0, len(address), row_length):
+            content.add_widget(Label(text=address[i:i + row_length]))
+        content.add_widget(btnclose)
+        popup = Popup(content=content, title='Copied Address',
+                      size_hint=(None, None), size=('300dp', '300dp'))
+        btnclose.bind(on_release=popup.dismiss)
+        button = Button(text='Open popup', size_hint=(None, None),
+                        size=('150sp', '70dp'),
+                        on_release=popup.open)
+        popup.open()
+        col = AnchorLayout()
+        col.add_widget(button)
+        return col
+
+
+class BalanceSection(BoxLayout):
+    def __init__(self, **kwargs):
+        super(BalanceSection, self).__init__(**kwargs)
+
+        main_layout = BoxLayout(orientation='horizontal')
+
+        # TODO wire up the real amount to a formatter which gets fed here
+        main_layout.add_widget(Label(text='1,276.00 mBTC', font_size=28, bold=True))
+
+        self.add_widget(main_layout)
+
+class TranscationSection(BoxLayout):
+    def __init__(self, **kwargs):
+        super(TranscationSection, self).__init__(**kwargs)
+
+        main_layout = BoxLayout(orientation='vertical')
+
         # TODO populate this list with real transaction data
-        transaction_history = ListView(item_strings=get_transactions(0,100))
+        transaction_history = ListView(item_strings=self.get_transactions(0,50))
         main_layout.add_widget(transaction_history)
-        ### end transaction section
+
+        self.add_widget(main_layout)
+
+    def get_transactions(self, start=0, amount=100):
+        item_strings=[('row ' + str(index) + ': +500 mBTC sent to 1MNmTP...') for index in range(amount)]
+
+        return item_strings
+
+
+class MainApp(App):
+
+    def build(self):
+        root = RootWidget()
+
+        main_layout = BoxLayout(orientation='vertical')
+
+
+
+        main_layout.add_widget(BalanceSection(size_hint_y=0.4))
+        main_layout.add_widget(ReceiveSection())
+        main_layout.add_widget(SendSection(size_hint_y=0.7))
+        main_layout.add_widget(TranscationSection(size_hint_y=1))
+
+
 
         root.add_widget(main_layout)
 
@@ -133,4 +159,5 @@ if __name__ == '__main__':
 
     seedphrase = sys.argv[1].encode("hex")
     print seedphrase
+    backend = backend.Backend(seedphrase)
     MainApp().run()
